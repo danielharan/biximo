052818e4aee7ad42216859435ae7ae5696583ce4
Daniel Haran <chebuctonian@mgmail.com>
2009-07-01 19:16:09 -0400

    move animation over to using marshalled objects

--- processing/map.rb
+++ processing/map.rb
@@ -3,63 +3,54 @@ require 'station'
 
 class Map < Processing::App
   BOUNDS = {:left_lng => -73.62831115722656, :right_lng => -73.52531433105469, :top_lat => 45.5560111391413, :low_lat => 45.4838452797276}
+  DAY    = "2009_06_30"
 
-  attr_accessor :stations, :img, :current_frame
+  attr_accessor :data_files, :img, :current_frame
   def setup
     color_mode RGB, 1.0
+    
+    @font = create_font('Helvetica', 40)
+    text_font @font
+    
     smooth
-    frame_rate 12
+    frame_rate 2
     @current_frame = 0
 
     @img  = load_image("montreal_gmap.gif")
     background @img
     
-    # JSON animation that was done for the web.
-    # Processing has its own JRuby distro, and both Nokogiri and HPricot are $%^& pain
-    # this is an ugly hack, but it's lazy and it works for now
-    @stations = eval(IO.read("./data/2009_06_24").gsub(":", "=>")).collect do |datum|
-      a = AnimatedStation.new datum
-      a.draw(@current_frame)
-      a
-    end
+    @data_files = Dir.glob("./data/#{DAY}*marshalled")
   end
   
   def draw
     return if @current_frame >= 288
     background @img
-    @stations.each {|station| station.draw(@current_frame)}
+    
+    fill 0, 0, 0, 1
+    # a tick is 5 minutes
+    hour, min = [$app.current_frame / 12, ($app.current_frame % 12) * 5].collect {|i| i.to_s.rjust(2, '0')}
+    text("#{DAY.gsub('_', ' ')} #{hour}:#{min}", 270, 35);
+    
+    loaded = Marshal.load(IO.read(@data_files[@current_frame])).each do |station|
+      AnimatedStation.new(station).draw
+    end
+    
     @current_frame += 1
   end
   
   class AnimatedStation
-    attr_accessor :options
-    def initialize(options)
-      @options = options # latitude, longitude, timeline
-      @x, @y = convert_lat_lng_to_relative_position
+    def initialize(station)
+      @station = station
     end
     
-    def draw(frame)
-      fill 0.7, 0, 0, @options["timeline"][frame]
-      oval @x * 600, @y * 600, 7, 7
-    end
-    
-    def convert_lat_lng_to_relative_position
-      [
-        (BOUNDS[:left_lng] - @options["longitude"].to_f) / (BOUNDS[:left_lng] - BOUNDS[:right_lng]), 
-        (BOUNDS[:top_lat]  - @options["latitude"].to_f)  / (BOUNDS[:top_lat]  - BOUNDS[:low_lat])
-      ]
+    def draw
+      fill 0.7, 0, 0, @station.fill_ratio.to_f
+      
+      x, y = @station.convert_lat_lng_to_relative_position(BOUNDS)
+      size = (Math.log(@station.size) / 1.94591014905531) * 12
+      ellipse x * 600, y * 600, size, size
     end
   end
 end
 
 Map.new(:width => 600, :height => 600, :title => "Montreal Bixi Map")
-
-# Info about the static map:
-# >>> map.getBounds().getSouthWest()
-# (45.4838452797276, -73.62831115722656) Ye=45.4838452797276 Ua=-73.62831115722656
-# >>> map.getBounds().getNorthEast()
-# (45.5560111391413, -73.52531433105469) Ye=45.5560111391413 Ua=-73.52531433105469
-# 
-# 1 - parse a kml file
-# 2 - place stations on a map
-# 3 - ensure you can adjust a station's fill
